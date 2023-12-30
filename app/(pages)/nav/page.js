abc82e70a1d678b878316362d40112e91d9504ee
vaggoban <mrg25mrg25@gmail.com>
2023-12-30 18:01:10 +0600

    one

--- app/(pages)/nav/page.js
+++ app/(pages)/nav/page.js
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client"
+ import React, { Component } from 'react';
+ import Image from 'next/image'
+import menu from "../../img/menu.png"
 
-export default function nav() {
-  return (
-    <div>
-        nav
-    </div>
-  )
+ class MyComponent extends Component {
+
+componentDidMount() {
+
 }
+
+  render() {
+    function ok(){
+      var element = document.getElementById("myDIV");
+      element.classList.toggle("mystyle");
+    }
+ return (
+      <>    
+        <div className="nav flex flex-row justify-between px-4 py-2 bg-gray-300">
+            <div className="logo uppercase tracking-wider style font-bold bg-cyan-400 rounded px-2 py-1">better call nicola</div>
+            <div className="menu h-8 w-8" onClick={ok}>
+                <Image alt='o' src={menu}></Image>
+            </div>
+        </div>
+        <div id="myDIV" className='hidden'>pp</div>
+
+      </>
+     );
+   }
+ }
+ 
+ export default MyComponent;
+
