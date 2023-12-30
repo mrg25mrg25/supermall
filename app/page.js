11498ea0c9133990382a2a5fffdb6eacfacb7899
vaggoban <mrg25mrg25@gmail.com>
2023-12-30 11:32:02 +0600

    maybe

--- app/page.js
+++ app/page.js
@@ -3,7 +3,38 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      fuck off
+      
+<div className="home px-4 capitalize sm:grid sm:grid-cols-2">
+
+
+<div className="part border border-rose-400">
+  <p className='text-red-800 text-sm py-6 '>mega sale, don't miss it out</p>
+  <div className="text-3xl">
+  <p className='normal-case'>All your needs</p>
+  <p className='normal-case'>are in one place</p>
+  </div>
+  <div className="text py-4">
+    <p>discount upto</p>
+    <p className='text-green-400 text-3xl' >20%off</p>
+    </div>
+  <div className="border border-rose-400 rounded px-3 py-2 cursor-pointer w-fit">shop now</div>
+ </div>
+
+ <div className="part border border-rose-400">
+  <p className='text-red-800 text-sm py-6 '>mega sale, don't miss it out</p>
+  <div className="text-3xl">
+  <p className='normal-case'>All your needs</p>
+  <p className='normal-case'>are in one place</p>
+  </div>
+  <div className="text py-4">
+    <p>discount upto</p>
+    <p className='text-green-400 text-3xl' >20%off</p>
+    </div>
+  <div className="border border-rose-400 rounded px-3 py-2 cursor-pointer w-fit">shop now</div>
+ </div>
+ 
+
+</div>
     </div>
   )
 }
