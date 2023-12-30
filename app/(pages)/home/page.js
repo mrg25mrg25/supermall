9b9c955b01ebd32f28565dd6746f36a2076e2397
vaggoban <mrg25mrg25@gmail.com>
2023-12-30 20:17:34 +0600

    q2

--- app/(pages)/home/page.js
+++ app/(pages)/home/page.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import Nav from "../../(pages)/nav/page"
+import Image from 'next/image'
+import Nicola from "../../img/nicola (1).jpeg"
 export default function home() {
   return (
-    <div>
+    <div className=''>
       <Nav/>
-      homem
+      <div className="home bg-gray-300 px-4 pt-8 sm:grid sm:grid-cols-2 pb-4 "id='opacity'>
+        <div className="left-text">
+        <div className="text ">
+          <p>WEB DEVELOPER</p>
+          <p className='capitalize text-3xl'>hello, it's</p>
+          <p className='capitalize text-3xl'>nicola goodman</p>
+        </div>
+        <div className="text">
+          <p className=''>I help construction companies build their online credibility by designing and developing websites</p>
+        </div>
+        </div>
+        <div className="right-text  flex justify-center items-center">
+          <div className="image_here sm:border bg-gray-200">
+            <div className="image_here_extra_border sm:border  mt-4 sm:px-12 sm:py-12 bg-gray-200">
+            <Image alt='' src={Nicola} className='  w-96 h-fit rounded-lg'></Image>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- app/(pages)/nav/page.js
+++ app/(pages)/nav/page.js
@@ -14,15 +14,19 @@ componentDidMount() {
       var element = document.getElementById("myDIV");
       element.classList.toggle("mystyle");
     }
+    function opacity(){
+      var element = document.getElementById("opacity");
+      element.classList.toggle("blur");
+    }
  return (
       <>    
         <div className="nav flex flex-row justify-between px-4 py-2 bg-gray-300">
-            <div className="logo uppercase tracking-wider style font-bold bg-cyan-400 rounded px-2 py-1">better call nicola</div>
+            <div className="logo uppercase tracking-wider style font-bold bg-gray-400 rounded px-2 py-1">better call nicola</div>
             <div className="menu h-8 w-8" onClick={ok}>
-                <Image alt='o' src={menu}></Image>
+                <Image alt='o' src={menu} onClick={opacity}></Image>
             </div>
         </div>
-        <div id="myDIV" className='hidden'>pp</div>
+        <div id="myDIV" className='hidden px-4'>pp</div>
 
       </>
      );
